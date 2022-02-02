58c96a152af8fcadadcc79036f1300f4dca6927f
Patrycja Miekina <patmiekina@gmail.com>
2022-02-02 00:49:55 +0000

    /about * /contact route

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -8,6 +8,14 @@ app.get("/", function(req, res){
   res.send("<h1>Hello, world</h1>");
 });
 
+app.get("/contact", function(req, res){
+  res.send("Contact me at myemail@gmail.com");
+});
+
+app.get("/about", function(req, res){
+  res.send("My name is Patrycja and I love flamingoes! 🦩🦩🦩");
+});
+
 app.listen(3000, function(){
   console.log("Server started on port 3000");
 });
