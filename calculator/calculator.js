8257e59fff0c86aa9139f95776bf0118d10c7cbf
Patrycja Miekina <patmiekina@gmail.com>
2022-02-02 23:07:48 +0000

    finished addition calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,8 +1,10 @@
 //jshint eversion:6
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
   // res.send("Hello, world!");
@@ -11,7 +13,14 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-  res.send("Thanks for posting that!");
+
+  var n1 = Number(req.body.n1);
+  var n2 = Number(req.body.n2);
+
+  var result = n1 + n2;
+
+  res.send("The result of the calculation is " + result);
+
 });
 
 app.listen(3000, function(){
