9e8939b204f698aa5e6d7957e6ef6b880e113f80
Patrycja Miekina <patmiekina@gmail.com>
2022-02-06 18:30:18 +0000

    Initial skeleton for proper site + body-parser installed

--- weatherProject/app.js
+++ weatherProject/app.js
@@ -7,49 +7,38 @@ const app = express();
 
 
 app.get("/", function(req, res){
-
-  // Danger!
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=152a29170995d42763b446749e9fbfed&units=metric";
-
-  https.get(url, function(response){
-
-    // console.log(response.statusCode);
-
-    response.on("data", function(data){
-
-      // Use JSON to parse the data
-      const weatherData = JSON.parse(data);
-
-      const temperature = weatherData.main.temp;
-
-      const weatherDesctiption = weatherData.weather[0].description;
-
-      // console.log(weatherData);
-
-      // console.log(temperature);
-
-      res.write("<h1>The temperature in London is " + temperature + " degrees Celsius.</h1>");
-
-      res.write("<p>The weather is currently " + weatherDesctiption + ".</p>");
-
-      // insert icon
-      const iconUrl = "http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png";
-
-      res.write("<img src=" + iconUrl + ">");
-
-      res.send();
-      // Use JSON to turn object into string with no spaces etc
-      // const object = {
-      //   name: "Angela",
-      //   favouriteFood: "Ramen"
-      // }
-
-      // console.log(JSON.stringify(object));
-
-    });
-  });
-
+  res.sendFile(__dirname + "/index.html")
 });
+
+app.post("/", function(req, res){
+  console.log("Post request received");
+});
+
+  // // search parameters
+  // const query = "London";
+  // const apiKey ="152a29170995d42763b446749e9fbfed";
+  // const units = "metric";
+  // // Danger!
+  // const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + units;
+
+  // https.get(url, function(response){
+
+  //   response.on("data", function(data){
+
+  //     // Use JSON to parse the data
+  //     const weatherData = JSON.parse(data);
+  //     const temperature = weatherData.main.temp;
+  //     const weatherDesctiption = weatherData.weather[0].description;
+  //     const iconUrl = "http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png";
+
+  //     res.write("<h1>The temperature in London is " + temperature + " degrees Celsius.</h1>");
+  //     res.write("<p>The weather is currently " + weatherDesctiption + ".</p>");
+  //     res.write("<img src=" + iconUrl + ">");
+
+  //     res.send();
+
+  //   });
+  // });
 
 app.listen(3000, function(){
 
