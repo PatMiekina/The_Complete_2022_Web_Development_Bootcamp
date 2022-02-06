142f579bb63576349ce40e4d66fff42d7c823c3d
Patrycja Miekina <patmiekina@gmail.com>
2022-02-06 17:43:42 +0000

    paring json

--- weatherProject/app.js
+++ weatherProject/app.js
@@ -8,10 +8,32 @@ const app = express();
 
 app.get("/", function(req, res){
 
+  // Danger!
   const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=152a29170995d42763b446749e9fbfed&units=metric";
 
   https.get(url, function(response){
-    console.log(response);
+
+    // console.log(response.statusCode);
+
+    response.on("data", function(data){
+
+      // Use JSON to parse the data
+      const weatherData = JSON.parse(data);
+
+      const temperature = weatherData.main.temp;
+
+      console.log(weatherData);
+      console.log(temperature);
+
+      // Use JSON to turn object into string with no spaces etc
+      // const object = {
+      //   name: "Angela",
+      //   favouriteFood: "Ramen"
+      // }
+
+      // console.log(JSON.stringify(object));
+
+    });
   })
 
   res.send("Server is up and running");
