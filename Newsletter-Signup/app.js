8748880776241e4e4b52938657ceb4df4ec78bb0
Patrycja Miekina <patmiekina@gmail.com>
2022-02-07 21:40:05 +0000

    email being collected

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,11 +1,29 @@
+// jshint esversion: 6
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 const app = express();
 
-app.get("/", function(req, res){
-  res.send("Hi there");
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
 });
 
-app.listen("3000", function(){
+app.post("/", function(req, res) {
+
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  console.log(firstName);
+  console.log(lastName);
+  console.log(email);
+
+});
+
+app.listen(3000, function() {
   console.log("The server is running on port 3000");
 });
