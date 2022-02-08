84112bf257d969d3e8c2908f5be0cfdef506cfae
Patrycja Miekina <patmiekina@gmail.com>
2022-02-08 21:00:02 +0000

    CSS added to failure.html & success.html

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -12,13 +12,24 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 mailchimp.setConfig({
-  apiKey: "70240db7b5d508947af2d65a3426c26a-us14",
+  apiKey: "MY_API KEY",
   server: "us14"
 });
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
+
+// For testing purposes
+app.get("/success", function(req, res) {
+  res.sendFile(__dirname + "/success.html");
+});
+
+app.get("/failure", function(req, res) {
+  res.sendFile(__dirname + "/failure.html");
+});
+
+
 
 app.post("/", function(req, res){
 
@@ -29,63 +40,73 @@ app.post("/", function(req, res){
       email: req.body.email
   };
 
-  // // Old solution - deprecated
-  // const data = {
-  //   members: [
-  //     {
-  //       email_address: subscribingUser.email,
-  //       status: "subscribed",
-  //       merge_fields: {
-  //         FNAME: subscribingUser.fName,
-  //         LNAME: subscribingUser.lName
-  //       }
-  //     }
-  //   ]
-  // };
-
-  // const jsonData = JSON.stringify(data);
-  // const url = "https://us14.api.mailchimp.com/3.0/lists/da6a2c7a44/members"
-  // // const url = "https://us14.api.mailchimp.com/3.0/lists/da6a2c7a44"
-
-  // const options = {
-  //   method: "POST",
-  //   auth: "patrycja:70240db7b5d508947af2d65a3426c26a-us14"
-  // }
-
-  // const request = https.request(url, options, function(response) {
-  //   response.on("data", function(){
-  //     // console.log(JSON.parse(data));
-  //   });
-  // });
-
-  // request.write(jsonData);
-  // request.end();
-  // // End of old solution
-
-  // New solution
-  async function run() {
-      try {
-          const response = await mailchimp.lists.addListMember(listId, {
-            email_address: subscribingUser.email,
-            status: "subscribed",
-            merge_fields: {
-              FNAME: subscribingUser.fName,
-              LNAME: subscribingUser.lName
-            }
-          });
-
-          console.log(
-            `Successfully added contact as an audience member. The contact's id is ${response.id}.`
-          );
-
-          res.sendFile(__dirname + "/success.html");
-      } catch (e) {
-          res.sendFile(__dirname + "/failure.html");
+  // Old solution - deprecated
+  const data = {
+    members: [
+      {
+        email_address: subscribingUser.email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: subscribingUser.fName,
+          LNAME: subscribingUser.lName
+        }
       }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+  const url = "https://us14.api.mailchimp.com/3.0/lists/da6a2c7a44/members"
+  // const url = "https://us14.api.mailchimp.com/3.0/lists/da6a2c7a44"
+
+  const options = {
+    method: "POST",
+    auth: "patrycja:743a5d540e61c43f888cec9addba4aef-us14"
   }
 
-  run();
-  // End of new solution
+  const request = https.request(url, options, function(response) {
+
+    if (response.statusCode == 200) {
+      console.log(`Successfully added contact as an audience member. The contact's id is ${response.id}.`);
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function(){
+      // console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+
+
+  // End of old solution
+
+  // // New solution
+  // async function run() {
+  //     try {
+  //         const response = await mailchimp.lists.addListMember(listId, {
+  //           email_address: subscribingUser.email,
+  //           status: "subscribed",
+  //           merge_fields: {
+  //             FNAME: subscribingUser.fName,
+  //             LNAME: subscribingUser.lName
+  //           }
+  //         });
+
+  //         console.log(
+  //           `Successfully added contact as an audience member. The contact's id is ${response.id}.`
+  //         );
+
+  //         res.sendFile(__dirname + "/success.html");
+  //     } catch (e) {
+  //         res.sendFile(__dirname + "/failure.html");
+  //     }
+  // }
+
+  // run();
+  // // End of new solution
 
 });
 
@@ -98,7 +119,7 @@ app.listen(3000, function() {
 });
 
 // API Key
-// 70240db7b5d508947af2d65a3426c26a-us14
+// 743a5d540e61c43f888cec9addba4aef-us14
 
 // Audience/List id
 // da6a2c7a44
